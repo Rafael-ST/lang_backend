b426907a064c2fe89f92743f4eac17030ee91ff5
Rafael Santos Teixeira <rafaelsantost87@gmail.com>
2026-06-10 16:36:59 -0300

    update model categoria

--- categorias/models.py
+++ categorias/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from app.models import BaseModel
 
 class Categoria(BaseModel):
-    nome = models.CharField(max_length=255, verbose_name='Nome da Categoria')
+    nome = models.CharField(max_length=255, verbose_name='Nome da Categoria', unique=True)
 
     def __str__(self):
         return self.nome
