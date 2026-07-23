b728967764da1ffab3dcb588d0a2c4cb283e336c
Rafael Santos <rafaelsantost87@gmail.com>
2026-07-23 10:54:33 -0300

    secret

--- lang_backend/settings.py
+++ lang_backend/settings.py
@@ -25,7 +25,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-zlhhdog8n(7!x&0q31km_yzs_*p3fdb8-je=6e3n9=o-cl)$j@'
-SECRET_KEY = os.getenv("SECRET_KEY")
+# SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
